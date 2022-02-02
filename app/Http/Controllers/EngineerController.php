736b859b209f3fd10d8f074924b7a017a019d73e
mHanash <zenasmomonzo@gmail.com>
2022-02-02 01:45:01 +0100

    review bug

--- app/Http/Controllers/EngineerController.php
+++ app/Http/Controllers/EngineerController.php
@@ -103,7 +103,7 @@ class EngineerController extends Controller
         }
         if (empty($request->img_url)) {
             if ($engineer->img_url == "") {
-                $pathImage = "/engineers/default.png";
+                $pathImage = "/default.png";
             }else {
                 $pathImage = $engineer->img_url;
             }
