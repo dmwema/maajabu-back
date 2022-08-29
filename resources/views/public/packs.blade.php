5cfc523831b599a3e34124fe7a6545ffbc1b301a
BN-Mara <sheltonsflash@gmail.com>
2022-08-29 12:20:46 +0100

    add sliding pack list

--- resources/views/public/packs.blade.php
+++ resources/views/public/packs.blade.php
@@ -4,10 +4,34 @@
 $page = 'services';
 $title = $service->title;
 @endphp
-
+@section('style')
+<link rel="stylesheet" type="text/css" href="{{asset('slick/slick.css')}}"/>
+<link rel="stylesheet" type="text/css" href="{{asset('slick/slick-theme.css')}}"/>
+<style>
+    #slider-container .your-class {
+    overflow: initial;
+}
+</style>
+@endsection
 @section('content')
+<section class="page-name v-separator">
+        <div class="dark-line"></div>
+        <div class="page-name-content">
+            <div class="container">
+                <h1>Plans et prix</h1>
+                <ul class="bread-crumbs">
+                    <li><a href="{{ route('public.home') }}">Accueil</a></li>
+                    <li>
+                        <p>Services</p>
+                    </li>
+                </ul>
+            </div>
+            <img src="data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=" class="page-name__bg lazy"
+                data-src="./img/page-name_bg.jpg" alt="bg">
+        </div>
+    </section>
     <section class="section services-section" id="services">
-        <div class="container">
+        <div class="container" id="slider-container">
             <div class="row">
                 <div class="col-lg-6">
                     <div class="section-title">
@@ -16,21 +40,27 @@ $title = $service->title;
                     </div>
                 </div>
             </div>
-            <div class="row">
+            <div class="your-class" >
                 @foreach ($packs as $pack)
                     <!-- feaure box -->
-                    <div class="col-sm-6 col-lg-4">
-                        <div class="feature-box-1">
+                    <div class="">
+                        <div class="feature-box-1" style="height: 50vh; border: 1px dotted #b07a12;">
                             <div class="feature-content">
-                                <h4>{{ $pack->title }}</h4>
+                                <h4 style="color: #b07a12">{{ $pack->title }}</h4>
                                 <div class="description">{!! $pack->description !!}</div>
-                                <hr>
+                                
+                                <div style="bottom: 100px;position:absolute">
                                 <p
-                                    style="font-weight: bold;color: #fff;background-color: red;  display: inline-block;padding: 5px 10px;border-radius: 4px;">
+                                    style="font-weight: bold;color: #fff;background-color: green;  display: inline-block;padding: 5px 10px;border-radius: 4px;">
                                     {{ $pack->price }} $
-                                </p><br>
+                                </p>
+                                </div>
+                                <div style="bottom: 10px;position:absolute;right: 0; left: 0;">
+                                <center>
                                 <a href="{{ route('public.new_reservation', ['pack_id' => $pack->id]) }}"
-                                    class="btn btn-primary mt-4">Réserver</a>
+                                    class="btn btn-primary mt-4" >Réserver</a>
+                                </center>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -38,7 +68,77 @@ $title = $service->title;
                 @endforeach
 
             </div>
+            
+
+            
+        </div>
+        <br><br>
+       <!-- <div class="container your-class">
+    <div>your content</div>
+    <div>your content</div>
+    <div>your content</div>
+  </div>-->
+    </section>
+
+    <section class="page-name v-separator">
+        <div class="dark-line"></div>
+        <div class="page-name-content">
+            <div class="container">
+                <ul class="bread-crumbs">
+                    <li>Nous vous proposons le Meuilleur Tarif! </li>
+                </ul>
+            </div>
+            <img src="data:image/gif;base64,R0lGODlhAQABAAAAACwAAAAAAQABAAA=" class="page-name__bg lazy"
+                data-src="./img/page-name_bg.jpg" alt="bg">
         </div>
     </section>
+<br><br>
+@endsection
+@section('script')
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="{{asset('slick/slick.min.js')}}"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+      $('.your-class').slick({
+        dot: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        speed: 300,
+        arrows:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+      });
+    });
+  </script>
 
 @endsection
